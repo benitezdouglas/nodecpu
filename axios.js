3035559799174017fe06025df2a6b9ba34c9814a
Douglas Benitez <douglasfazion@gmail.com>
2022-10-14 20:29:07 -0300

    validate seconds

---
+++ axios.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+var moment = require('moment');
+
+// Make a request for a user with a given ID
+axios.get('https://api.github.com/repos/benitezdouglas/nodecpu/commits')
+  .then(function (response) {
+    // handle success
+    const firstCommit = response.data.length - 1;
+    const lastCommit = 0;
+
+    var firstCommitDate = moment(response.data[firstCommit].commit.committer.date); // another date
+    var lastCommitDate = moment(response.data[lastCommit].commit.committer.date); //todays date
+    var duration = moment.duration(lastCommitDate.diff(firstCommitDate));
+
+    console.log(response.data[firstCommit].commit.message, firstCommitDate);
+    console.log(response.data[lastCommit].commit.message, lastCommitDate);
+
+    console.log(duration.asMinutes());   
+    console.log(duration.asSeconds());   
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
